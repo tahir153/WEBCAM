d5489a28b5a6578522b3079ff09ca0ba9df69495
Tahir Habib <93587612+tahir153@users.noreply.github.com>
2022-07-17 08:32:59 +0500

    Update webcam.py

--- webcam.py
+++ webcam.py
@@ -1,8 +1,10 @@
 import cv2
-cap = cv2.VideoCapture(0)
+cap = cv2.VideoCapture(1)
+cap.set(3,1366) # you can set here the window size
+cap.set(4,768)
 while True:
     success, img = cap.read()
     cv2.imshow('camera', img)
     if cv2.waitKey(10) == ord('q'):
-        break
+        break # press 'Q' key to close the window
 cv2.waitKey(1)
